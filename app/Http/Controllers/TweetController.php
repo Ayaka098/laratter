7df28fe503cbdfcadc4166b3078414e586ff5983
Ayaka <a.m.0626se3@gmail.com>
2025-09-05 20:19:28 +0900

    3回目講義完全版

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -12,7 +12,9 @@ class TweetController extends Controller
      */
     public function index()
     {
-        //
+        // 🔽 追加
+    $tweets = Tweet::with('user')->latest()->get();
+    return view('tweets.index', compact('tweets'));
     }
 
     /**
@@ -20,7 +22,8 @@ class TweetController extends Controller
      */
     public function create()
     {
-        //
+        // 🔽 追加
+        return view('tweets.create');
     }
 
     /**
@@ -29,6 +32,13 @@ class TweetController extends Controller
     public function store(Request $request)
     {
         //
+        $request->validate([
+      'tweet' => 'required|max:255',
+    ]);
+
+    $request->user()->tweets()->create($request->only('tweet'));
+
+    return redirect()->route('tweets.index');
     }
 
     /**
@@ -37,6 +47,7 @@ class TweetController extends Controller
     public function show(Tweet $tweet)
     {
         //
+        return view('tweets.show', compact('tweet'));
     }
 
     /**
@@ -62,4 +73,6 @@ class TweetController extends Controller
     {
         //
     }
+    
+
 }
